8eaf7950df7e65596f3181c94f6e4a50c5406c65
IndiraKeerthana <p.indirakeerthana@gmail.com>
2025-09-14 16:25:53 +0530

    Only view

--- TaskTracker.java
+++ TaskTracker.java
@@ -40,19 +40,7 @@ public class TaskTracker {
             System.out.println("Invalid task number.\n");
         }
     }
-    //delete task
-    public static void deleteTask() {
-        viewTasks();
-        System.out.print("Enter task number to delete: ");
-        int index = Integer.parseInt(scanner.nextLine()) - 1;
-        
-        if (index >= 0 && index < tasks.size()) {
-            tasks.remove(index);
-            System.out.println("🗑️ Task deleted!\n");
-        } else {
-            System.out.println("⚠️ Invalid task number.\n"); 
-        }
-    }
+    
 
     public static void main(String[] args) {
         
