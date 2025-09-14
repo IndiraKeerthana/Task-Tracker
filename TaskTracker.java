7280b754cc0c3a4cd86873ec8932094e6a771c26
IndiraKeerthana <p.indirakeerthana@gmail.com>
2025-09-14 16:43:34 +0530

    Resolve conflict

--- TaskTracker.java
+++ TaskTracker.java
@@ -42,17 +42,17 @@ public class TaskTracker {
     }
     //delete task
     public static void deleteTask() {
-        viewTasks();
-        System.out.print("Enter task number to delete: ");
-        int index = Integer.parseInt(scanner.nextLine()) - 1;
-        
-        if (index >= 0 && index < tasks.size()) {
-            tasks.remove(index);
-            System.out.println("Task deleted!\n");
-        } else {
-            System.out.println("Invalid task number.\n"); 
-        }
+    viewTasks();
+    System.out.print("Enter task number to delete: ");
+    int index = Integer.parseInt(scanner.nextLine()) - 1;
+
+    if (index >= 0 && index < tasks.size()) {
+        tasks.remove(index);
+        System.out.println("Task deleted!\n");
+    } else {
+        System.out.println("Invalid task number.\n");
     }
+}
 
     public static void main(String[] args) {
         
